7f063746d10b459ef8391be91be044d6eef2e094
Mattherix <mattherix@protonmail.com>
2021-02-03 12:02:21 +0100

    Ajout d'un test vide pour testé l'action CI

---
+++ test/tmp.py
@@ -0,0 +1,8 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_toujours_vrai(self):
+        # TODO: A retirer lors que l'on aura des vrais testes
+        """Vérifie le fonctionnement des testes."""
+        self.assertEqual(True, True)
