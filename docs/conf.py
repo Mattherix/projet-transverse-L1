caddfc05564c23cc945d9fc968f234f7112097b2
Mattherix <mattherix@protonmail.com>
2021-02-05 22:26:36 +0100

    Disable extensions docs

--- docs/conf.py
+++ docs/conf.py
@@ -28,8 +28,7 @@ author = 'Matthieu Roquejoffre'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.githubpages'
+
 ]
 
 # Add any paths that contain templates here, relative to this directory.
