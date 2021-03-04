50dda571eb4defea989c648f094823fd571878b8
Mattherix <mattherix@protonmail.com>
2021-03-04 08:59:48 +0100

    Ajout de l'exemple de base

--- src/main.py
+++ src/main.py
@@ -1,12 +1,26 @@
-"""
-Le fichier principale, c'est la clef de voute du programme.
-"""
+import sys, time, pygame
 
+pygame.init()
 
-def main():
-    """La fonction src contenant l'intégralité du programme.
+size = width, height = 1000, 500
+speed = [2, 2]
+black = 0, 0, 0
+screen = pygame.display.set_mode(size)
+ball = pygame.image.load("intro_ball.gif")
+ballrect = ball.get_rect()
 
-    :type: int
-    :return: Rien
-    """
-    pass
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    time.sleep(0.01)
+    pygame.display.flip()
