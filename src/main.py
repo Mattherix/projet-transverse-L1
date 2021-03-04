b05b6c0473798df500b89fe4f8ec6e84dee798be
Mattherix <mattherix@protonmail.com>
2021-03-04 15:57:10 +0100

    Ajout edit angle et vitesse

--- src/main.py
+++ src/main.py
@@ -55,7 +55,7 @@ display.init()
 
 
 class Perso:
-    def __init__(self, x, y, w, h, v=10, g=9.81, angle=1, color=(66, 73, 73)):
+    def __init__(self, x, y, w, h, v=10, g=9.81, angle=10, color=(66, 73, 73)):
         self.x = x
         self.y = y
         self.w = w
@@ -75,7 +75,8 @@ class Perso:
         :param angle: l'angle de portée, c'est-à-dire l'angle avec lequel le projectile est lancé, en degrés
         :return:
         """
-        return (- self.g / ((2 * self.v) ** 2) * ((cos(self.angle)) ** 2)) * (self.x ** 2) + tan(self.angle) * self.x
+        # return (- self.g) / ((2 * self.v) ** 2 * cos(self.angle) ** 2) * self.x ** 2 + tan(self.angle) * self.x
+        return -(- self.g / ((2 * self.v) ** 2) * ((cos(self.angle)) ** 2)) * (self.x ** 2) + tan(self.angle) * self.x
 
     def next_position(self, x):
         self.x = self.x + x
@@ -85,7 +86,7 @@ class Perso:
         pygame.draw.rect(surface, self.color, (self.x, self.y + self.h * 1 / 3, self.w, self.h * 2 / 3))
 
 import pygame
-
+from time import sleep
 pygame.init()
 
 pygame.display.set_caption("TheGame")
@@ -93,9 +94,15 @@ screen = pygame.display.set_mode((1080, 720))
 
 background = pygame.image.load('assets/space1.png')
 
+v = int(input("Vitesse: "))
+angle = int(input("Angle: "))
+sleep(3)
+
 surface = display.get_surface()
-p = Perso(100, 500, 20, 20)
+p = Perso(100, 500, 20, 20, v=v, angle=angle)
 clock = pygame.time.Clock()
+
+
 while True:
     # arriere plan du jeu,
     screen.blit(background, (0, 0))
