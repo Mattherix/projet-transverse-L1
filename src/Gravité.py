9fc587cbf11553003169f64b2502e189c206b22c
Mattherix <mattherix@protonmail.com>
2021-03-13 17:39:41 +0100

    Use PEP8 syntax

--- src/Gravité.py
+++ src/Gravité.py
@@ -5,19 +5,19 @@ import pygame
 from pygame.locals import *
 
 # On initialise pygame
-pygame.init ()
+pygame.init()
 taille_fenetre = (600, 400)
-fenetre_rect = pygame.Rect ((0, 0), taille_fenetre)
-screen_surface = pygame.display.set_mode (taille_fenetre)
+fenetre_rect = pygame.Rect((0, 0), taille_fenetre)
+screen_surface = pygame.display.set_mode(taille_fenetre)
 
 BLEU_NUIT = (5, 5, 30)
 VERT = (0, 255, 0)
 JAUNE = (255, 255, 0)
 
-timer = pygame.time.Clock ()
+timer = pygame.time.Clock()
 
-joueur = pygame.Surface ((25, 25))
-joueur.fill (JAUNE)
+joueur = pygame.Surface((25, 25))
+joueur.fill(JAUNE)
 # Position du joueur
 x, y = 25, 100
 # Vitesse du joueur
@@ -25,20 +25,20 @@ vx, vy = 0, 0
 # Gravité vers le bas donc positive
 GRAVITE = 2
 
-mur = pygame.Surface ((25, 25))
-mur.fill (VERT)
+mur = pygame.Surface((25, 25))
+mur.fill(VERT)
 
 niveau = [
-    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-    [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
-    [ 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 ],
-    [ 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 ],
-    [ 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1 ],
-    [ 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1 ],
-    [ 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1 ],
-    [ 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1 ],
-    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
 
 
@@ -47,10 +47,10 @@ def dessiner_niveau(surface, niveau):
 
     Utilise la surface `mur` pour dessiner les cases de valeur 1
     """
-    for j, ligne in enumerate (niveau):
-        for i, case in enumerate (ligne):
+    for j, ligne in enumerate(niveau):
+        for i, case in enumerate(ligne):
             if case == 1:
-                surface.blit (mur, (i * 25, j * 25))
+                surface.blit(mur, (i * 25, j * 25))
 
 
 def from_coord_to_grid(pos):
@@ -60,8 +60,8 @@ def from_coord_to_grid(pos):
     On limite i et j à être positif.
     """
     x, y = pos
-    i = max (0, int (x // 25))
-    j = max (0, int (y // 25))
+    i = max(0, int(x // 25))
+    j = max(0, int(y // 25))
     return i, j
 
 
@@ -73,12 +73,12 @@ def get_neighbour_blocks(niveau, i_start, j_start):
     la case à droite ou celle en bas et à droite. On ne prend en compte que
     les cases du niveau avec une valeur de 1.
     """
-    blocks = list ()
-    for j in range (j_start, j_start + 2):
-        for i in range (i_start, i_start + 2):
-            if niveau[ j ][ i ] == 1:
+    blocks = list()
+    for j in range(j_start, j_start + 2):
+        for i in range(i_start, i_start + 2):
+            if niveau[j][i] == 1:
                 topleft = i * 25, j * 25
-                blocks.append (pygame.Rect ((topleft), (25, 25)))
+                blocks.append(pygame.Rect((topleft), (25, 25)))
     return blocks
 
 
@@ -92,16 +92,16 @@ def bloque_sur_collision(niveau, old_pos, new_pos, vx, vy):
     La fonction retourne la position modifiée pour new_pos ainsi que les
     vitesses modifiées selon les éventuelles collisions.
     """
-    old_rect = pygame.Rect (old_pos, (25, 25))
-    new_rect = pygame.Rect (new_pos, (25, 25))
-    i, j = from_coord_to_grid (new_pos)
-    collide_later = list ()
-    blocks = get_neighbour_blocks (niveau, i, j)
+    old_rect = pygame.Rect(old_pos, (25, 25))
+    new_rect = pygame.Rect(new_pos, (25, 25))
+    i, j = from_coord_to_grid(new_pos)
+    collide_later = list()
+    blocks = get_neighbour_blocks(niveau, i, j)
     for block in blocks:
-        if not new_rect.colliderect (block):
+        if not new_rect.colliderect(block):
             continue
 
-        dx_correction, dy_correction = compute_penetration (block, old_rect, new_rect)
+        dx_correction, dy_correction = compute_penetration(block, old_rect, new_rect)
         # Dans cette première phase, on n'ajuste que les pénétrations sur un
         # seul axe.
         if dx_correction == 0.0:
@@ -111,20 +111,20 @@ def bloque_sur_collision(niveau, old_pos, new_pos, vx, vy):
             new_rect.left += dx_correction
             vx = 0.0
         else:
-            collide_later.append (block)
+            collide_later.append(block)
 
     # Deuxième phase. On teste à présent les distances de pénétrations pour
     # les blocks qui en possédaient sur les 2 axes.
     for block in collide_later:
-        dx_correction, dy_correction = compute_penetration (block, old_rect, new_rect)
+        dx_correction, dy_correction = compute_penetration(block, old_rect, new_rect)
         if dx_correction == dy_correction == 0.0:
             # Finalement plus de pénétration. Le new_rect a bougé précédemment
             # lors d'une résolution de collision
             continue
-        if abs (dx_correction) < abs (dy_correction):
+        if abs(dx_correction) < abs(dy_correction):
             # Faire la correction que sur l'axe X (plus bas)
             dy_correction = 0.0
-        elif abs (dy_correction) < abs (dx_correction):
+        elif abs(dy_correction) < abs(dx_correction):
             # Faire la correction que sur l'axe Y (plus bas)
             dx_correction = 0.0
         if dy_correction != 0.0:
@@ -159,27 +159,27 @@ def compute_penetration(block, old_rect, new_rect):
 # Boucle événementielle
 continuer = True
 while continuer:
-    for event in pygame.event.get ():
+    for event in pygame.event.get():
         if event.type == QUIT:
             continuer = False
         elif event.type == KEYDOWN:
             if event.key == K_SPACE:
                 vy = -20
 
-    timer.tick (30)
-    keys_pressed = pygame.key.get_pressed ()
+    timer.tick(30)
+    keys_pressed = pygame.key.get_pressed()
     # Sauvegarde de l'ancienne position
     old_x, old_y = x, y
-    vx = (keys_pressed[ K_RIGHT ] - keys_pressed[ K_LEFT ]) * 5
+    vx = (keys_pressed[K_RIGHT] - keys_pressed[K_LEFT]) * 5
     vy += GRAVITE
-    vy = min (20, vy)  # vy ne peut pas dépasser 25 sinon effet tunnel...
+    vy = min(20, vy)  # vy ne peut pas dépasser 25 sinon effet tunnel...
     x += vx
     y += vy
-    x, y, vx, vy = bloque_sur_collision (niveau, (old_x, old_y), (x, y), vx, vy)
+    x, y, vx, vy = bloque_sur_collision(niveau, (old_x, old_y), (x, y), vx, vy)
 
-    screen_surface.fill (BLEU_NUIT)
-    dessiner_niveau (screen_surface, niveau)
-    screen_surface.blit (joueur, (x, y))
-    pygame.display.flip ()
+    screen_surface.fill(BLEU_NUIT)
+    dessiner_niveau(screen_surface, niveau)
+    screen_surface.blit(joueur, (x, y))
+    pygame.display.flip()
 
-pygame.quit ()
+pygame.quit()
