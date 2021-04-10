70d6d723f2b496251a07ceba5bca7521b0da08ff
Mattherix <mattherix@protonmail.com>
2021-04-10 16:33:29 +0200

    Retrait des fichiers inutilisés dans le programme

--- src/main.py
+++ src/main.py
@@ -1,74 +1,58 @@
-from math import cos, tan
-from time import sleep
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import pygame
-from catapulte_jauge import Catapulte, Jauge
 from pygame import display
+from pygame.locals import QUIT, KEYDOWN, K_SPACE
+
+from catapulte_jauge import Jauge
+from src.dessin import dessiner_niveau
+from src.joueur import Joueur
+from src.settings import NIVEAU, TAILLE_FENETRE, TITRE_FENETRE, FPS
 
 pygame.init()
 
-display.init()
+
+pygame.mixer.init()
+pygame.mixer.music.load("music_and_sound/[04] Flowering Night.flac")
+pygame.mixer.music.play(-1)
 
 
-class Perso:
-    def __init__(self, x, y, w, h, v=10, g=9.81, angle=10, color=(66, 73, 73)):
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
-        self.v = v
-        self.g = g
-        self.angle = angle
-        self.h = h
-        self.color = color
+fenetre_rect = pygame.Rect((0, 0), TAILLE_FENETRE)
+screen_surface = pygame.display.set_mode(TAILLE_FENETRE)
+display.set_caption(TITRE_FENETRE)
 
-    def pos_projectile(self):
-        """Position du projectile
+timer = pygame.time.Clock()
 
-        :param x: x(t) = v * cos(a) * t
-        :param v: la vitesse initiale, à laquelle le projectile est lancé, en m/s
-        :param g: l'accélération de la pesanteur en m/s2 (9,81 m/s2 à la surface de la Terre)
-        :param angle: l'angle de portée, c'est-à-dire l'angle avec lequel le projectile est lancé, en degrés
-        :return:
-        """
-        # return (- self.g) / ((2 * self.v) ** 2 * cos(self.angle) ** 2) * self.x ** 2 + tan(self.angle) * self.x
-        return -(- self.g / ((2 * self.v) ** 2) * ((cos(self.angle)) ** 2)) * (self.x ** 2) + tan(self.angle) * self.x
-
-    def next_position(self, x):
-        self.x = self.x + x
-        self.y = self.pos_projectile()
-
-    def draw(self, surface, loaded=None):
-        pygame.draw.rect(surface, self.color, (self.x, self.y + self.h * 1 / 3, self.w, self.h * 2 / 3))
-
-
-display.set_caption("TheGame")
-screen = display.set_mode((1080, 720))
 
 background = pygame.image.load('assets/space1.png')
+# Position du joueur
+x, y = 25, 100
 
-v = int(input("Vitesse: "))
-angle = int(input("Angle: "))
-sleep(3)
-
-surface = display.get_surface()
-catapulte = Catapulte()
+joueur = Joueur(x, y, 20, 20)
 jauge = Jauge()
-p = Perso(100, 500, 20, 20, v=v, angle=angle)
-clock = pygame.time.Clock()
 
-while True:
-    # arriere plan du jeu,
-    screen.blit(background, (0, 0))
-
+# Boucle événementielle
+continuer = True
+while continuer:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            print("End")
+        if event.type == QUIT:
+            continuer = False
+        elif event.type == KEYDOWN:
+            if event.key == K_SPACE:
+                vy = -20
 
-    p.next_position(1)
-    p.draw(surface)
+    # Update de la position
+    keys_pressed = pygame.key.get_pressed()
+    joueur.position(keys_pressed)
 
-    # mettre a jour l'écran
-    time = clock.tick(60)
-    display.update()
+    # Dessin
+    screen_surface.blit(background, (0, 0))
+    dessiner_niveau(screen_surface, NIVEAU)
+    joueur.draw(screen_surface)
+
+
+    pygame.display.flip()
+    timer.tick(FPS)
+
+pygame.quit()
